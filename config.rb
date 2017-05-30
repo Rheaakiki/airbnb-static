998a82cd92f289b36d74f417c9d98df6fba09af2
Rhea Akiki <rhea.akiki@gmail.com>
2017-05-30 09:12:02 +0200

    third

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 ###
 # Compass
 ###
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 
 # Change Compass configuration
 # compass_config do |config|
